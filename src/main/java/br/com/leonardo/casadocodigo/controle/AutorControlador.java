3f72ab12b9f232985a1a5cd64dd99525bc57fe0c
junioryes13 <leojunior1326@gmail.com>
2021-08-23 21:42:31 -0300

    Ajustes na classe autor , onde foi removida a classe de repositorio para um melhor entendimento do código

--- src/main/java/br/com/leonardo/casadocodigo/controle/AutorControlador.java
+++ src/main/java/br/com/leonardo/casadocodigo/controle/AutorControlador.java
@@ -3,27 +3,27 @@ package br.com.leonardo.casadocodigo.controle;
 
 import br.com.leonardo.casadocodigo.dto.AutorForm;
 import br.com.leonardo.casadocodigo.modelo.Autor;
-import br.com.leonardo.casadocodigo.repositorio.AutorRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
 import javax.validation.Valid;
-import java.time.LocalDateTime;
-import java.util.List;
 
 @RestController
 @RequestMapping("/Autor")
 public class AutorControlador {
 
     @Autowired
-   private AutorRepositorio autorRepositorio;
+    EntityManager entityManager;
 
     @PostMapping
+    @Transactional
     public ResponseEntity<Autor> Salvar (@RequestBody @Valid AutorForm form){
         Autor autorSalvo = form.converterParaEntidade();
-        autorRepositorio.save(autorSalvo);
+        entityManager.persist(autorSalvo);
         return ResponseEntity.status(HttpStatus.OK).body(autorSalvo);
     }
 }
--- src/main/java/br/com/leonardo/casadocodigo/dto/AutorForm.java
+++ src/main/java/br/com/leonardo/casadocodigo/dto/AutorForm.java
@@ -1,16 +1,10 @@
 package br.com.leonardo.casadocodigo.dto;
 
 import br.com.leonardo.casadocodigo.modelo.Autor;
-import br.com.leonardo.casadocodigo.repositorio.AutorRepositorio;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.time.LocalDateTime;
 
 public class AutorForm {
 
